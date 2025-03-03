0b970a6157e12256d33995d217d76108704911f7
尤雨东 <root@ltpp.vip>
2025-03-03 21:46:13 +0800

    feat: v3.10.0

--- src/stream/type.rs
+++ src/stream/type.rs
@@ -9,10 +9,10 @@ pub type ArcRwLockWriteGuardTcpStream<'a> = Arc<RwLockWriteGuard<'a, TcpStream>>
 pub type OptionArcRwLockWriteGuardTcpStream<'a> = Option<ArcRwLockWriteGuardTcpStream<'a>>;
 pub type ArcMutexGuardTcpStream<'a> = Arc<MutexGuard<'a, TcpStream>>;
 pub type OptionArcMutexGuardTcpStream<'a> = Option<ArcMutexGuardTcpStream<'a>>;
-pub type ClientHost = IpAddr;
-pub type ClientPort = u16;
-pub type OptionClientHost = Option<IpAddr>;
-pub type OptionClientPort = Option<u16>;
+pub type SocketHost = IpAddr;
+pub type SocketPort = u16;
+pub type OptionSocketHost = Option<SocketHost>;
+pub type OptionSocketPort = Option<SocketPort>;
 pub type OptionSocketAddr = Option<SocketAddr>;
 
 #[derive(Clone, Debug)]
