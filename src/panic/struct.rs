53d11a1ad23ab9947f80795644016bd4fa7c6b0f
sheng <root@ltpp.vip>
2026-05-12 09:17:37 +0800

    feat: v19.0.0

--- src/panic/struct.rs
+++ src/panic/struct.rs
@@ -6,31 +6,15 @@ use crate::*;
 /// source code location, and payload. It is used by the server's panic handling
 /// mechanism and passed to the configured panic hook for custom processing.
 #[derive(
-    Clone,
-    CustomDebug,
-    Default,
-    Deserialize,
-    DisplayDebug,
-    Eq,
-    Getter,
-    PartialEq,
-    Serialize,
-    Setter,
-    New,
+    Clone, CustomDebug, Default, Deserialize, DisplayDebug, Eq, Getter, PartialEq, Serialize, New,
 )]
 pub struct PanicData {
     /// The message associated with the panic.
     /// This is `None` if the panic payload is not a string.
-    #[get(pub)]
-    #[set(pub(crate))]
     pub(super) message: Option<String>,
     /// The source code location where the panic occurred.
-    #[get(pub)]
-    #[set(pub(crate))]
     pub(super) location: Option<String>,
     /// The payload of the panic, often a string literal.
     /// The hook attempts to downcast it to a `&str` or `String`.
-    #[get(pub)]
-    #[set(pub(crate))]
     pub(super) payload: Option<String>,
 }
